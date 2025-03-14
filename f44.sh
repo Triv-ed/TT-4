dd8f5fe2f830652f07112770678ad9bf0a8fdce0
EC2 Default User <ec2-user@ip-172-31-9-228.ap-south-1.compute.internal>
2025-03-14 15:34:11 +0000

    3 and 7 added

---
+++ f44.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Enter a number: " num
+if (( num % 3 == 0 && num % 7 == 0 )); then
+    echo "$num is divisible by both 3 and 7"
+else
+    echo "$num is not divisible by both 3 and 7"
+fi
+
